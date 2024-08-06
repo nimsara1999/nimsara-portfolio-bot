0e159e246fccbb8c04d00a1150370380f706dca0
Nimsara Thisal Paramulla <85945422+nimsara1999@users.noreply.github.com>
2024-08-07 01:43:21 +0530

    initial commit

---
+++ src/main/java/com/nimsara/nimsaraportfoliobot/NimsaraPortfolioBotApplication.java
@@ -0,0 +1,13 @@
+package com.nimsara.nimsaraportfoliobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimsaraPortfolioBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NimsaraPortfolioBotApplication.class, args);
+    }
+
+}
