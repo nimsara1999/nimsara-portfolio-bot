5d255b4fb16506190eda45482dc61ab97fe8e368
Nimsara Thisal Paramulla <85945422+nimsara1999@users.noreply.github.com>
2024-08-07 18:03:59 +0530

    Implemented basic chatbot functionalities with api

--- src/main/java/com/nimsara/nimsaraportfoliobot/NimsaraPortfolioBotApplication.java
+++ src/main/java/com/nimsara/nimsaraportfoliobot/NimsaraPortfolioBotApplication.java
@@ -1,10 +1,42 @@
 package com.nimsara.nimsaraportfoliobot;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.loadDocument;
+
 @SpringBootApplication
 public class NimsaraPortfolioBotApplication {
+
+    private final EmbeddingStoreIngestor embeddingStoreIngestor;
+
+    public NimsaraPortfolioBotApplication(EmbeddingStoreIngestor embeddingStoreIngestor) {
+        this.embeddingStoreIngestor = embeddingStoreIngestor;
+    }
+
+    @PostConstruct
+    public void init() {
+        Document document = loadDocument(toPath("cv.pdf"), new ApachePdfBoxDocumentParser());
+        embeddingStoreIngestor.ingest(document);
+    }
+
+    private static Path toPath(String fileName) {
+        try {
+            URL fileUrl = NimsaraPortfolioBotApplication.class.getClassLoader().getResource(fileName);
+            return Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NimsaraPortfolioBotApplication.class, args);
---
+++ src/main/java/com/nimsara/nimsaraportfoliobot/PdfAssistantConfig.java
@@ -0,0 +1,67 @@
+package com.nimsara.nimsaraportfoliobot;
+
+
+import dev.langchain4j.chain.ConversationalRetrievalChain;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.retriever.EmbeddingStoreRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.cassandra.AstraDbEmbeddingConfiguration;
+import dev.langchain4j.store.embedding.cassandra.AstraDbEmbeddingStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PdfAssistantConfig {
+    @Value("${chatgpt.key}")
+    private String apiKey;
+
+    @Value("${astra.astraToken}")
+    private String astraToken;
+
+    @Value("${astra.databaseId}")
+    private String databaseId;
+
+    @Value("${astra.keySpace}")
+    private String keySpace;
+
+    @Bean
+    public AllMiniLmL6V2EmbeddingModel embeddingModel() {
+        return new AllMiniLmL6V2EmbeddingModel();
+    }
+
+    @Bean
+    public AstraDbEmbeddingStore astraDbEmbeddingStore() {
+
+        return new AstraDbEmbeddingStore(AstraDbEmbeddingConfiguration
+                .builder()
+                .token(astraToken)
+                .databaseId(databaseId)
+                .databaseRegion("us-east1")
+                .keyspace(keySpace)
+                .table("myData")
+                .dimension(384)
+                .build());
+    }
+
+    @Bean
+    public EmbeddingStoreIngestor embeddingStoreIngestor() {
+        return EmbeddingStoreIngestor.builder()
+                .documentSplitter(DocumentSplitters.recursive(300, 0))
+                .embeddingModel(embeddingModel())
+                .embeddingStore(astraDbEmbeddingStore())
+                .build();
+    }
+
+    @Bean
+    public ConversationalRetrievalChain conversationalRetrievalChain() {
+
+
+        return ConversationalRetrievalChain.builder()
+                .chatLanguageModel(OpenAiChatModel.withApiKey(apiKey))
+                .retriever(EmbeddingStoreRetriever.from(astraDbEmbeddingStore(), embeddingModel()))
+                .build();
+    }
+}
